8f44ed079bf92aacf2c3a5d9d7ff16cd5c293e99
Sam2836 <samiacobazzi@gmail.com>
2025-01-04 17:49:30 -0500

    Implement weights/biases randomizer and unit network test

--- net.py
+++ net.py
@@ -1,3 +1,12 @@
+from mnist import MNIST
+
+mndata = MNIST("samples")
+
+images, labels = mndata.load_training()
+images, labels = list(images), list(labels)
+
+
+
 import numpy as np
 
 def sigmoid(x):
@@ -11,8 +20,59 @@ H_LAYER_COUNT = 2
 
 class neuron:
     def __init__(self):
-        self.weights_in = [None]*H_LAYER_SIZE
+        self.weights_in = []
         self.bias = None
         self.output = None
     
 h_layers = [[neuron()]*H_LAYER_SIZE for i in range(H_LAYER_COUNT)]
+
+input_layer = [neuron() for i in range(784)]
+
+final_layer = [neuron()]*10
+
+# print(mndata.display(images[0]))
+
+def randomize_weights_and_biases():
+    r = np.random.default_rng()
+    
+    for neuron in h_layers[0]:
+        for i in range(0,784):
+            neuron.weights_in.append(r.random())
+        neuron.bias = r.random()
+            
+    for layer in h_layers[1:]:
+        for neuron in layer:
+            for i in range(H_LAYER_SIZE):
+                neuron.weights_in.append(r.random())
+            neuron.bias = r.random()
+        
+    for neuron in final_layer:
+        for i in range(H_LAYER_SIZE):
+            neuron.weights_in.append(r.random())
+        neuron.bias = r.random()
+
+
+def run_one_image(images, index):
+    for i in range(0,784):
+        input_layer[i].output = images[index][i]
+        
+    # Populate 1st hidden layer
+    for i in range(0,H_LAYER_SIZE):
+        h_layers[0][i].output = sigmoid(sum(
+            [input_layer[j].output * h_layers[0][i].weights_in[j] for j in range(0,784)]
+            ) + h_layers[0][i].bias)
+        
+    # Populate other hidden layers
+    for i in range(1,H_LAYER_COUNT):
+        for j in range(0,H_LAYER_SIZE):
+            h_layers[i][j].output = sigmoid(sum(
+                [h_layers[i-1][k].output * h_layers[i][j].weights_in[k] for k in range(0,H_LAYER_SIZE)]
+                ) + h_layers[i][j].bias)
+    
+    # Populate final layer
+    for i in range(0,10):
+        final_layer[i].output = sigmoid(sum(
+            [h_layers[H_LAYER_COUNT-1][j].output * final_layer[i].weights_in[j] for j in range(0,H_LAYER_SIZE)]
+            ) + final_layer[i].bias)
+        
+    return max(enumerate([final_layer[i].output for i in range(0,10)]))[0]
